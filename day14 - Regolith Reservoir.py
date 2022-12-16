d36b728232d17fbf5cfcd2e854b578c9f95a8d89
ruthcaswellsmith <ruth@plannuh.com>
2022-12-15 20:18:09 -0500

    Day 14

--- day14 - Regolith Reservoir.py
+++ day14 - Regolith Reservoir.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
-from enum import Enum, auto
-from typing import List, Union, Tuple
+from enum import Enum
+from typing import List, Union
 import numpy as np
 import pandas as pd
-
-from functools import total_ordering
 
 from utils import read_file, XYPair, Part
 
@@ -17,23 +15,20 @@ class Occupant(str, Enum):
     SAND = 0
 
 
-class DirToFall(str, Enum):
-    LEFT = auto()
-    DOWN = auto()
-    RIGHT = auto()
-
-
 class Sand:
     def __init__(self, xypair: XYPair):
         self.pos = xypair
 
+    def __get_occupants(self, grid: np.array):
+        return [grid[self.pos.x+1, y] for y in range(self.pos.y-1, self.pos.y+2)]
+
     def is_blocked(self, grid: np.array) -> bool:
-        occupants = [grid[self.pos.x+1, y] for y in range(self.pos.y-1, self.pos.y+2)]
-        return all(occupants)
+        occupants = self.__get_occupants(grid)
+        return sum([pd.isna(occupant) for occupant in occupants]) == 0
 
     def get_new_pos(self, grid: np.array) -> Union[None, XYPair]:
         options = [XYPair((self.pos.x+1, y)) for y in range(self.pos.y-1, self.pos.y+2)]
-        occupants = [grid[self.pos.x+1, y] for y in range(self.pos.y-1, self.pos.y+2)]
+        occupants = self.__get_occupants(grid)
         return options[1] if pd.isna(occupants[1]) else \
             options[0] if pd.isna(occupants[0]) else \
             options[2] if pd.isna(occupants[2]) else \
@@ -42,6 +37,7 @@ class Sand:
 
 class Reservoir:
     def __init__(self, data: List[str], part: Part):
+        self.part = part
         self.data = data
         self.sand = Sand(XYPair((0, 500)))
         self.segments = self.__get_segments()
@@ -52,20 +48,28 @@ class Reservoir:
             self.max_depth += 2
         self.grid = np.nan*np.zeros((self.max_depth + 1, SIZE))
         self.__add_rocks()
-        self.process()
 
     @property
-    def answer_pt1(self):
-        return np.count_nonzero(self.grid == 0) - 1
-
-    def process(self, part: Part):
-        if part == Part.PT1:
-            falling_into_abyss = False
-            while not falling_into_abyss:
-                self.sand = Sand(XYPair((0, 500)))
-                falling_into_abyss = self.fall()
+    def answer(self):
+        if self.part == Part.PT1:
+            return np.count_nonzero(self.grid == 0) - 1
         else:
-            while not
+            return np.count_nonzero(self.grid == 0) + 1
+
+    def process(self):
+        falling_into_abyss = False
+        self.sand = Sand(XYPair((0, 500)))
+        while not falling_into_abyss and not self.sand.is_blocked(self.grid):
+            falling_into_abyss = self.fall()
+            self.sand = Sand(XYPair((0, 500)))
+
+    def fall(self):
+        new_pos = self.sand.get_new_pos(self.grid)
+        while new_pos and self.sand.pos.x < self.max_depth - 1:
+            self.sand.pos.update(new_pos)
+            new_pos = self.sand.get_new_pos(self.grid)
+        self.grid[self.sand.pos.x, self.sand.pos.y] = Occupant.SAND
+        return True if new_pos else False
 
     def __add_rocks(self):
         for segment in self.segments:
@@ -80,20 +84,15 @@ class Reservoir:
             segments.extend([(points[i], points[i+1]) for i in range(len(points)-1)])
         return segments
 
-    def fall(self):
-        new_pos = self.sand.get_new_pos(self.grid)
-        while new_pos and self.sand.pos.x < self.max_depth - 1:
-            self.sand.pos.update(new_pos)
-            new_pos = self.sand.get_new_pos(self.grid)
-        self.grid[self.sand.pos.x, self.sand.pos.y] = Occupant.SAND
-        return True if new_pos else False
-
 
 if __name__ == '__main__':
     filename = 'input/day14.txt'
     data = read_file(filename)
 
     reservoir = Reservoir(data, Part.PT1)
-    print(f"The answer to Pt 1 is {reservoir.answer_pt1}")
+    reservoir.process()
+    print(f"The answer to Pt 1 is {reservoir.answer}")
 
     reservoir = Reservoir(data, Part.PT2)
+    reservoir.process()
+    print(f"The answer to Pt 2 is {reservoir.answer}")

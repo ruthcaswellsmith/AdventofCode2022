35598a799d98ea7c10e9c91c45cbe43c3fafcd7b
ruthcaswellsmith <ruth@plannuh.com>
2022-12-17 19:22:48 -0500

    Day 15

--- day16 - Proboscidea Volcanium.py
+++ day16 - Proboscidea Volcanium.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 from utils import read_file
 LARGE = 1_000_000
 
-NC = namedtuple('ND', 'name cost')
+NC = namedtuple('NC', 'name cost')
 
 
 class Node:
@@ -13,108 +13,77 @@ class Node:
         self.name = name
         self.adj_list = adj_list
         self.cost = LARGE
-        self.visited = False
 
 
 class Graph:
     def __init__(self, nodes: List[Node]):
         self.nodes = nodes
-        self.shortest_paths: Dict[Node, List[NC]] = {}
+        self.shortest_paths: Dict[str, List[NC]] = {}
+        self.visited = List[Node]
 
     @property
     def num_nodes(self):
         return len(self.nodes)
 
+    @property
+    def unvisited_nodes(self):
+        unvisited = [n for n in self.nodes if n not in self.visited]
+        unvisited.sort(key=lambda n: n.cost)
+        return unvisited
+
     @staticmethod
     def __get_edge_cost(adj_list: List[NC], node: Node):
-        return next(iter(t.edge_cost for t in adj_list if t.name == node.name), 0)
+        return next(iter(t.cost for t in adj_list if t.name == node.name), 0)
 
     def get_node(self, name: str):
         return next(iter([n for n in self.nodes if n.name == name]), None)
 
     def find_all_shortest_paths(self):
         for node in self.nodes:
-            self.shortest_paths[node] = self.__find_shortest_paths(node)
+            self.shortest_paths[node.name] = self.__find_shortest_paths(node)
 
     def __find_shortest_paths(self, starting_node: Node):
-        current, visited = starting_node, []
+        current = starting_node
+        self.visited = []
         for n in self.nodes:
             n.cost = LARGE
-            n.visited = False
         current.cost = 0
 
         done = False
         while not done:
-            neighbors = self.__get_unvisited_neighbors(current, visited)
-            self.__update_costs_for_neighbors(neighbors)
-            visited.append(current)
-            if not len(visited) == self.num_nodes:
-                self.__update_current(visited)
+            neighbors = self.__get_unvisited_neighbors(current)
+            self.__update_costs_for_neighbors(current, neighbors)
+            self.visited.append(current)
+            if not len(self.visited) == self.num_nodes:
+                current = self.unvisited_nodes[0]
             else:
                 done = True
-        return [ND(name=v.name, distance=v.cost) for v in visited if v.name != starting_node.name]
+        return [NC(name=n.name, cost=n.cost) for n in self.nodes if n.name != starting_node.name]
 
-    def __get_unvisited_neighbors(self, current: Node, visited: List[Node]) -> List[Node]:
-        return [self.get_node(n.name) for n in current.adj_list if n not in visited]
+    def __get_unvisited_neighbors(self, current: Node) -> List[Node]:
+        return [self.get_node(n.name) for n in current.adj_list if n not in self.visited]
 
     def __update_costs_for_neighbors(self, current: Node, neighbors: List[Node]):
         for neighbor in neighbors:
             neighbor.cost = min(neighbor.cost, current.cost + self.__get_edge_cost(current.adj_list, neighbor))
 
-    def __update_current(self, visited: List[Node]):
-        unvisited_neighbors = [n for n in self.nodes if not n.visited]
-        unvisited_neighbors.sort(key=lambda n: n.cost)
-        return unvisited_neighbors[0]
 
-
-
-# class Valve:
-#     def __init__(self, line: str):
-#         self.name = line[line.index('Valve ')+6:line.index('Valve ')+8]
-#         self.flow_rate = eval(line[line.index('=')+1:line.index(';')])
-#         self.adj_list = line[line.index("valves ")+7:].split() if 'valves' in line else \
-#             line[line.index("valve ") + 6:].split()
-#         self.adj_list = [n.replace(',', '') for n in self.adj_list]
-#         self.cost = LARGE
-#         self.visited = False
+class Valve(Node):
+    def __init__(self, line: str):
+        name = line[line.index('Valve ')+6:line.index('Valve ')+8]
+        adj_nodes = line[line.index("valves ")+7:].split() if 'valves' in line else \
+            line[line.index("valve ") + 6:].split()
+        adj_list = [NC(n.replace(',', ''), 1) for n in adj_nodes]
+        super().__init__(name, adj_list)
+        self.flow_rate = eval(line[line.index('=')+1:line.index(';')])
 
 
 class Tunnels:
     def __init__(self, data: List[str]):
-        self.valves = [Valve(line) for line in data]
-        self.tsp_valves = [v for v in self.valves if v.name == 'AA'] + [v for v in self.valves if v.flow_rate > 0]
-        for v in self.tsp_valves:
-            self.__find_shortest_paths(v)
-        self.build_tsp_graph()
-        print('h')
+        self.graph_with_all_valves = Graph([Valve(line) for line in data])
+        self.graph_with_all_valves.find_all_shortest_paths()
+        self.nonzero_valves = [v for v in self.graph_with_all_valves.nodes if v.flow_rate > 0]
 
-    def build_tsp_graph(self):
-        for v in self.tsp_valves:
-            # for n in self.tsp_valves:
-            #     if n.name != v.name:
-            #         print('h')
-            v.adj_list = [(n.name, abs(n.cost - v.cost)) for n in self.tsp_valves if n.name != v.name]
-            print('h')
-
-    @property
-    def num_valves(self):
-        return len(self.valves)
-
-    @property
-    def visited_valves(self):
-        return len([v for v in self.valves if v.visited])
-
-    def __find_shortest_paths(self, starting_valve: Valve):
-        current, visited = starting_valve, []
-        done = False
-        while not done:
-            neighbors = self.__get_unvisited_neighbors(current, visited)
-            self.__update_costs_for_neighbors(neighbors)
-            visited.append(current)
-            if not len(visited) == self.num_valves:
-                self.__update_current(visited)
-            else:
-                done = True
 
 
 if __name__ == '__main__':

84fc5fee61083fa0e70b4b084e6ff14654c534e9
ruthcaswellsmith <ruth@plannuh.com>
2022-12-29 11:39:20 -0500

    Day 16

--- day16 - Proboscidea Volcanium.py
+++ day16 - Proboscidea Volcanium.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
-from typing import List, Tuple, Dict
-from collections import namedtuple
-from itertools import permutations
+from typing import List, Tuple
 import numpy as np
+import itertools as it
 
 from utils import read_file, Graph, GraphNode
-LARGE = 1_000_000
 
 
 class Valve:
@@ -25,43 +23,63 @@ class Tunnels:
         self.nodes = [(GraphNode(i, [self.name_to_id[n] for n in v.adj_valves])) for i, v in enumerate(self.valves)]
         self.graph = Graph(self.nodes, np.logical_not(np.identity(len(self.nodes))).astype(int))
         self.graph.find_all_shortest_paths()
-        self.tsp_valves = [v for v in self.valves if v.flow_rate > 0 or v.name == 'AA']
-        self.current_max = -1_000_000
-        self.find_max_flow(self.__get_valve('AA'))
-        self.visited = []
-        self.total_cost = 0
+        self.shortest_paths = self.__shortest_paths()
+        self.visited_states = {}
 
-    def __get_unvisited_neighbors(self):
-        return [n for n in self.tsp_valves if n not in self.visited]
+    @staticmethod
+    def two_partitions(S):
+        res_list = []
+        for l in range(0, int(len(S) / 2) + 1):
+            combis = set(it.combinations(S, l))
+            for c in combis:
+                res_list.append((sorted(list(c)), sorted(list(S - set(c)))))
+        return res_list
 
-    def find_max_flow(self, valve: Valve):
-        self.visited.append(valve)
-        neighbors = self.__get_unvisited_neighbors()
-        if len(neighbors) == 2:
-            return self.graph.shortest_paths[]
+    def find_max_with_elephant(self):
+        permutations = self.two_partitions(set([v for v in tunnels.shortest_paths if v != 'AA']))
+        maxval = 0
+        for i, p in enumerate(permutations):
+            maxval = max(maxval, tunnels.find_max(26, 'AA', tuple(p[0])) + tunnels.find_max(26, 'AA', tuple(p[1])))
+        return maxval
 
+    def find_max(self, time: int, valve: str, valves: Tuple[str]):
+        if (time, valve, valves) in self.visited_states:
+            return self.visited_states[(time, valve, valves)]
 
-    def __get_valve(self, name: str):
+        maxval = 0
+        for target in self.shortest_paths[valve]:
+            if target in valves:
+                continue
+            rem_time = time - self.shortest_paths[valve][target] - 1
+            if rem_time <= 0:
+                continue
+            maxval = max(maxval, self.find_max(rem_time, target, valves + (target,)) + \
+                         self.get_valve(target).flow_rate * rem_time)
+
+        self.visited_states[(time, valve, valves)] = maxval
+        return maxval
+
+    def __shortest_paths(self):
+        shortest_paths = {}
+        for v in self.valves:
+            if v.flow_rate == 0 and v.name != 'AA':
+                continue
+            shortest_paths[v.name] = {
+                target.name: self.graph.shortest_paths[v.id, target.id] for target in self.valves if \
+                target.flow_rate > 0 and target.name != v.name
+            }
+        return shortest_paths
+
+    def get_valve(self, name: str):
         return next(iter(v for v in self.valves if v.name == name))
-
-    def __takes_too_long(self, path: List[Valve]):
-        total_time = 0
-        for i in range(len(path)-1):
-            total_time += self.graph.shortest_paths[path[i].id, path[i+1].id] + 1
-            if total_time > 30:
-                return True
-
-    def __get_cost(self, path: List[Valve]):
-        total_time, total_cost = 0, 0
-        for i in range(len(path) - 1):
-            total_time += self.graph.shortest_paths[path[i].id, path[i+1].id] + 1
-            total_cost += path[i+1].flow_rate * (30 - total_time)
-        return total_cost
 
 
 if __name__ == '__main__':
-    filename = 'input/day16.txt'
+    filename = 'input/test16.txt'
     data = read_file(filename)
 
     tunnels = Tunnels(data)
-    print('h')
+    print(f'The answer to part 1 is {tunnels.find_max(30, "AA", ())}')
+
+    tunnels = Tunnels(data)
+    print(f'The answer to part 2 is {tunnels.find_max_with_elephant()}')
